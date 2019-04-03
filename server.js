5cada94545a43b3506cebb639af52fbf16ac7c3b
Yash shah <Yash9060@users.noreply.github.com>
2019-04-03 00:05:12 -0700

    Working updates
    All Tabs

--- server.js
+++ server.js
@@ -29,7 +29,7 @@ app.get('/productinfo', function(req, res) {
     console.log(url);
     axios.get(url)
         .then(function(response) {
-            console.log(response);
+            //console.log(response);
             res.send(response.data);
         })
         .catch(function(error) {
@@ -38,3 +38,34 @@ app.get('/productinfo', function(req, res) {
         })
 
 });
+
+app.get('/getpics', function(req, res) {
+
+    var URI = req.query.params;
+    console.log("************************************************************")
+    console.log(URI)
+    console.log("********************************************************");
+    axios.get(URI)
+        .then(function(response) {
+            console.log('Do I send data');
+            res.send(response.data);
+        })
+        .catch(function(error) {
+            console.log(error);
+        })
+
+});
+
+app.get('/getsimilaritems', function(req, res) {
+    console.log('In similar Items')
+    var itemid = req.query.params
+    var url = "http://svcs.ebay.com/MerchandisingService?OPERATION-NAME=getSimilarItems&SERVICE-NAME=MerchandisingService&SERVICE-VERSION=1.1.0&CONSUMER-ID=YashShah-Yash-PRD-e16e2f149-341d2218&RESPONSE-DATA-FORMAT=JSON&REST-PAYLOAD&itemId=" + itemid + "&maxResults=20";
+
+    axios.get(url)
+        .then(function(response) {
+            //console.log(response);
+            res.send(response.data);
+        }).catch(function(reserrorponse) {
+            console.log(error)
+        })
+})
