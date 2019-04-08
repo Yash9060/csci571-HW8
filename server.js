d76091f023e0d65cd8b48bd2b8bbf12e434fdaaf
Yash shah <Yash9060@users.noreply.github.com>
2019-04-08 14:43:34 -0700

    Add files via upload

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 var express = require('express');
+var request = require('request');
 var axios = require('axios');
 var app = express();
 app.use(express.static("myApp"));
@@ -7,19 +8,27 @@ console.log('MyProject Server is Listening on port 8000');
 
 app.get('/', function(req, res) {
     console.log('Got response');
-    /*  */
+    
     var url = req.query.params;
-    console.log(url);
+    /*console.log(url);
     axios.get(url)
         .then(function(response) {
-            console.log("response");
+            console.log("response from server");
             res.send(response.data);
 
         })
         .catch(function(error) {
             // handle error
+            console.log("Error..");
             console.log(error);
-        })
+        })*/
+
+        request(url, function (error, response, body) {
+  //console.log('error:', error); // Print the error if one occurred
+  //console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
+  //console.log('body:', body); // Print the HTML for the Google homepage.
+  res.send(body);
+});
 
 });
 
@@ -38,6 +47,17 @@ app.get('/productinfo', function(req, res) {
         })
 
 });
+// app.get('/autocomplete', function(req,res){
+// axios.get("http://api.geonames.org/postalCodeSearchJSON?postalcode_startsWith=900&username="+req.query.params6+"&country=US&maxRows=5")
+// .then(function(response){
+//     res.send(response.data)
+// }).
+// catch(function(error){
+//     console.log(error);
+// })
+
+
+// });
 
 app.get('/getpics', function(req, res) {
 
@@ -65,7 +85,21 @@ app.get('/getsimilaritems', function(req, res) {
         .then(function(response) {
             //console.log(response);
             res.send(response.data);
-        }).catch(function(reserrorponse) {
+        }).catch(function(error) {
             console.log(error)
         })
-})
+});
+
+app.get('/autocomplete',function(req,res){
+console.log("In Autocomplete");
+
+var url1 = "http://api.geonames.org/postalCodeSearchJSON?postalcode_startsWith="+req.query.params6+"&username=yash9060&country=US&maxRows=5";
+axios.get(url1)
+    .then(function(response){
+        //console.log(response);
+        res.send(response.data)
+
+    }).catch(function(error){
+        console.log(error);
+    })
+});
